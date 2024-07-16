b578e8917323ed9fd480c276d0bf4f06f88a976c
jpshiro <162577448+1jpshiro@users.noreply.github.com>
2024-07-16 22:19:44 +0700

    .

--- Stat.py
+++ Stat.py
@@ -1,9 +1,13 @@
+#              © Copyright 2024
+#
+# 🔒      Licensed under the GNU AGPLv3
+# 🌐 https://www.gnu.org/licenses/agpl-3.0.html
 # meta developer: 猫ちゃん
 
-from telethon.tl.types import Message
 from .. import loader, utils
+from telethon.tl.types import Message
 
-class pmstat(loader.Module):
+class PMstat(loader.Module):
     """Counting how many messages did you and your chat partner write"""
 
     strings = {
@@ -16,29 +20,29 @@ class pmstat(loader.Module):
         """ [-p] [-s] - (-p - counts your chat partner messages) (-s - send result to the saved messages)"""
         args = utils.get_args_raw(message)
         if not message.is_private:
-            await utils.answer(message, self.strings['pm'])
+            await utils.answer(message, self.strings["pm"])
             return
 
         await message.delete()
         chat = await self.client.get_entity(message.peer_id.user_id)
-        w = "you" if "-p" not in args else f"<a href='tg://user?id={chat.id}'>{chat.first_name}</a>"
+        who = "you" if "-p" not in args else f"<a href='tg://user?id={chat.id}'>{chat.first_name}</a>"
         s = chat.id if "-s" not in args else self.tg_id
-        r = 0
-        _r = []
+        count = 0
+        msgsList = []
 
-        async for j in self.client.iter_messages(chat.id):
-            _r.append(j)
+        async for i in self.client.iter_messages(chat.id):
+            msgList.append(i)
 
         if "-p" in args:
-            for l in _r:
-                if l.from_id != self.tg_id:
-                    r += 1
+            for i in msgList:
+                if i.from_id != self.tg_id:
+                    count += 1
         else:
-            for p in _r:
-                if p.from_id == self.tg_id:
-                    r += 1
+            for j in _r:
+                if j.from_id == self.tg_id:
+                    count += 1
 
         await message.client.send_message(
             s,
-            self.strings['q'].format(r, w)
+            self.strings["q"].format(count, who)
         )
