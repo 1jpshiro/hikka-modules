7c47b7442c45abf6118ee4f7ab4a77c00f826080
jpshiro <162577448+1jpshiro@users.noreply.github.com>
2024-07-22 00:33:29 +0300

    Update PrivateCopy.py

--- PrivateCopy.py
+++ PrivateCopy.py
@@ -137,7 +137,9 @@ class PrivateImitator(loader.Module):
     async def imitatecmd(self, message: Message):
         """ [limit: int] - save all the media and messages from specified channel"""
         args = (utils.get_args_raw(message)).split()
-        limit = int(args[0]) if args[0].isdigit() else None
+        limit = None
+        if len(args) > 0:
+            limit = int(args[0]) if args[0].isdigit() else None
         yourChannel = self.config["your_channel"]
         someChannel = self.config["some_channel"]
         if not all(isinstance(i, Channel) for i in [
