11439b9212e4eb07f318a475aa0fbd914824c5c2
shiro <162577448+1jpshiro@users.noreply.github.com>
2024-08-27 12:00:40 +0700

    Counter.py

--- Counter.py
+++ Counter.py
@@ -2,7 +2,7 @@
 #
 # 🔒      Licensed under the GNU AGPLv3
 # 🌐 https://www.gnu.org/licenses/agpl-3.0.html
-# meta developer: 猫ちゃん(@shiro_hikka)
+# meta developer: @shiro_hikka
 
 from .. import loader, utils
 from telethon.tl.types import Message
@@ -12,7 +12,7 @@ import asyncio
 NAME = "Counter"
 
 class Counter(loader.Module):
-    """InlineCounter"""
+    """猫ちゃん | InlineCounter"""
 
     strings = {
         "name": "Counter",
