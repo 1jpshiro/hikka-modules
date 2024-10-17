be18d6cb2e8eaaf621f380435a7e723850e996a5
shiro <162577448+1jpshiro@users.noreply.github.com>
2024-10-17 15:53:34 +0700

    Opportunity to choose the time interval

--- DelMyMsgs.py
+++ DelMyMsgs.py
@@ -16,10 +16,27 @@ class DelMyMsgs(loader.Module):
     }
 
     async def purgecmd(self, message: Message):
-        """ [reply] - delete all your messages in current chat or only ones up to message you replyed to"""
+        """ [reply] [10s / 10m / 10h / 10d] - delete all your messages in current chat or only ones up to message you replyed to\nExample: 10h 3d"""
+        args = (utils.get_args_raw(message)).split()
+        time = 0
+
+        if args:
+            for i in args:
+                if len(i) < 2:
+                    continue
+
+                time += (
+                    int(i[:-1]) if i[-1] == "s"
+                    else int(i[:-1])*60 if i[-1] == "m"
+                    else int(i[:-1])*3600 if i[-1] == "h"
+                    else int(i[:-1])*86400 if i[-1] == "d"
+                    else 0
+                )
+
         reply = await message.get_reply_message()
         is_last = False
 
+        await asyncio.sleep(time)
         async for i in self.client.iter_messages(message.chat.id):
             if i.from_id == self.tg_id:
                 if reply:
