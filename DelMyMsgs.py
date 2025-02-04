3eddcb3a4b1f6e6910a0aed069053c4cde6c11d9
shiro <162577448+1jpshiro@users.noreply.github.com>
2025-02-04 14:32:22 +0700

    Update DelMyMsgs.py
    add a command to stop the deletion

--- DelMyMsgs.py
+++ DelMyMsgs.py
@@ -1,22 +1,58 @@
-#              © Copyright 2024
+# ---------------------------------------------------------------------------------
+# Author: @shiro_hikka
+# Name: Message Eraser
+# Description: Delete all your messages in the current chat
+# Commands: purge, stoppurge
+# ---------------------------------------------------------------------------------
+#              © Copyright 2025
 #
 # 🔒      Licensed under the GNU AGPLv3
 # 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+# ---------------------------------------------------------------------------------
+# scope: hikka_only
 # meta developer: @shiro_hikka
+# meta banner: https://0x0.st/s/FIR0RnhUN5pZV5CZ6sNFEw/8KBz.jpg
+# ---------------------------------------------------------------------------------
+
+__version__ = (1, 1, 0)
 
 from .. import loader, utils
 from telethon.tl.types import Message
 import asyncio
 
-class DelMyMsgs(loader.Module):
-    """猫ちゃん | Delete all your messages in current chat"""
+@loader.tds
+class MessageEraser(loader.Module):
+    """Delete all your messages in the current chat"""
 
     strings = {
-        "name": "DelMyMsgs"
+        "name": "MessageEraser",
+        "enabled": "<emoji document_id=5289755247298747469>😒</emoji> It doesn't operates now anyway",
+        "disabled": "<emoji document_id=5237870268541582966>❄️</emoji> Operation status changed to disabled"
     }
 
+    async def client_ready(self):
+        status = self.db.get(__name__, "status")
+        if status is None:
+            self.db.set(__name__, "status", False)
+
+
+    async def stoppurgecmd(self, message: Message):
+        """ Interupt the process of deletion"""
+        status = self.db.get(__name__, "status")
+        status = not status if status is True else status
+        self.db.set(__name__, "status", status)
+
+        if status is True:
+            await utils.answer(message, self.strings["disabled"])
+        else:
+            await utils.answer(message, self.strings["enabled"])
+
     async def purgecmd(self, message: Message):
-        """ [reply] [10s / 10m / 10h / 10d] - delete all your messages in current chat or only ones up to message you replyed to\nExample: 10h 3d"""
+        """
+        [reply] [10s / 10m / 10h / 10d] - delete all your messages in the current chat or only ones up to the message you replied to
+        Posible to do in a specific time
+        Example: 10h 3d
+        """
         args = (utils.get_args_raw(message)).split()
         time = 0
 
@@ -37,6 +73,8 @@ class DelMyMsgs(loader.Module):
         is_last = False
 
         await asyncio.sleep(time)
+        self.db.set(__name__, "status", True)
+
         async for i in self.client.iter_messages(message.chat.id):
             if i.from_id == self.tg_id:
                 if reply:
@@ -46,6 +84,4 @@ class DelMyMsgs(loader.Module):
                         is_last = True
                 await message.client.delete_messages(message.chat.id, [i.id])
 
-        q = await utils.answer(message, "<b>Done</b>")
-        await asyncio.sleep(2)
-        await q.delete()
+        await utils.answer(message, "<emoji document_id=5292186100004036291>🤩</emoji> Done")
